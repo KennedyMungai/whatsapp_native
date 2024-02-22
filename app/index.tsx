d2d53aee563aa8a9ef7526d0fc85c265ed417f1d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-22 12:38:35 +0300

    Made some changes in the markup and behaviour of the component

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,22 @@ import { Image, StyleSheet, Text, View } from 'react-native'
 const welcome_image = Image.resolveAssetSource(welcomeImage).uri
 
 const Page = () => {
+	const openLink = () => {}
+
 	return (
 		<View style={styles.container}>
 			<Image source={{ uri: welcome_image }} style={styles.welcome} />
 			<Text style={styles.headline}>Welcome to WhatsApp Native</Text>
+			<Text>
+				Read out{' '}
+				<Text style={styles.links} onPress={openLink}>
+					Privacy Policy
+				</Text>{' '}
+				.{'Tap "Agree & Continue" to accept the '}
+				<Text style={styles.links} onPress={openLink}>
+					Terms of Service
+				</Text>
+			</Text>
 		</View>
 	)
 }
