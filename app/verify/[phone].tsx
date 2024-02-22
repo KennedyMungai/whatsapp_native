c1d63179244ada40ada8ddef9df16966d0f66705
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-22 15:57:20 +0300

    Added a useEffect block that checks the length of the code every time it changes

--- app/verify/[phone].tsx
+++ app/verify/[phone].tsx
@@ -1,10 +1,26 @@
 import { useLocalSearchParams } from 'expo-router'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Text, View } from 'react-native'
+import {
+	CodeField,
+	Cursor,
+	useBlurOnFulfill,
+	useClearByFocusCell
+} from 'react-native-confirmation-code-field'
 
 const PhoneNumberPage = () => {
-	const { phone, signin } = useLocalSearchParams<{ phone: string, signin: string }>()
-    const [code, setCode] = useState('')
+	const { phone, signin } = useLocalSearchParams<{
+		phone: string
+		signin: string
+	}>()
+	const [code, setCode] = useState('')
+
+	useEffect(() => {
+		if (code.length === 6) {
+			console.log('Code', code)
+            // TODO: Verify code
+		}
+	}, [code])
 
 	return (
 		<View>
