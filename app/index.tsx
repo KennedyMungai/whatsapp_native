cf62acd2f1a6ca961f911c48b07cca2b4d2f7bac
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-22 12:31:31 +0300

    Set the background color to white

--- app/index.tsx
+++ app/index.tsx
@@ -19,7 +19,8 @@ const styles = StyleSheet.create({
 		flex: 1,
 		padding: 20,
 		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
+		backgroundColor: '#fff'
 	},
 	welcome: {
 		width: '100%',
