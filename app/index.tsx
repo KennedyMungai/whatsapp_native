bf07204fd706697a8dcd62ba844fbeaa21417b08
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-22 12:21:24 +0300

    Added some styling to the first page of the app

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Page = () => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text>First Page</Text>
 		</View>
 	)
 }
 
 export default Page
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+})
