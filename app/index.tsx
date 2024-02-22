d4a2a90c426aed7a55dc407860e9e776c28a0f58
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-22 12:54:05 +0300

    Added the asChild prop to the Link component

--- app/index.tsx
+++ app/index.tsx
@@ -22,7 +22,7 @@ const Page = () => {
 					Terms of Service
 				</Text>
 			</Text>
-			<Link href={'/otp'}>
+			<Link href={'/otp'} asChild>
 				<TouchableOpacity style={styles.button}>
 					<Text style={styles.buttonText}>Agree & Continue</Text>
 				</TouchableOpacity>
