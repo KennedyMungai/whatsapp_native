01eb77a2cc65052943df8df7b776c61227f0f462
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-22 14:18:10 +0300

    Some useState hooks and a router instance were defined

--- app/otp.tsx
+++ app/otp.tsx
@@ -1,6 +1,18 @@
-import { KeyboardAvoidingView, Text } from 'react-native'
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
+import { KeyboardAvoidingView, Linking, Platform, Text } from 'react-native'
 
 const OTPPage = () => {
+	const [loading, setLoading] = useState(false)
+	const [phoneNumber, setPhoneNumber] = useState('')
+	const router = useRouter()
+
+	const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 0
+
+	const openLink = () => {
+		Linking.openURL('https://www.wikipedia.org')
+	}
+
 	return (
 		<KeyboardAvoidingView>
 			<Text>OTPPage</Text>
