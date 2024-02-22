b07824372f2190b3b50a516edf61f06c20143eea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-22 16:05:14 +0300

    Made some changes to the phone componenrt

--- app/verify/[phone].tsx
+++ app/verify/[phone].tsx
@@ -1,6 +1,7 @@
+import Colors from '@/constants/Colors'
 import { useLocalSearchParams } from 'expo-router'
 import { useState, useEffect } from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import {
 	CodeField,
 	Cursor,
@@ -18,15 +19,61 @@ const PhoneNumberPage = () => {
 	useEffect(() => {
 		if (code.length === 6) {
 			console.log('Code', code)
-            // TODO: Verify code
+			// TODO: Verify code
 		}
 	}, [code])
 
+	const verifyCode = async () => {}
+
+	const verifySignIn = async () => {}
+
+	const resendCode = async () => {}
+
 	return (
-		<View>
-			<Text>{phone}</Text>
+		<View style={styles.container}>
+			<Text style={styles.legal}>
+				Read out{' '}
+				<Text style={styles.links} onPress={() => {}}>
+					Privacy Policy
+				</Text>{' '}
+				.{'Tap "Agree & Continue" to accept the '}
+				<Text style={styles.links} onPress={() => {}}>
+					Terms of Service
+				</Text>
+			</Text>
 		</View>
 	)
 }
 
 export default PhoneNumberPage
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		alignItems: 'center',
+		backgroundColor: Colors.background,
+		gap: 20
+	},
+	loading: {
+		backgroundColor: 'rgba(0,0,0,0.5)',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	legal: {
+		fontSize: 14,
+		textAlign: 'center',
+		color: '#000'
+	},
+	button: {
+		width: '100%',
+		alignItems: 'center'
+	},
+	buttonText: {
+		color: Colors.primary,
+		fontSize: 18
+	},
+	links: {
+		color: Colors.primary
+	}
+})
