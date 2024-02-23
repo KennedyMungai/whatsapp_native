14abee8b244c153af7825704673a07cb44635e78
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-23 10:35:16 +0300

    Implemented ClerkProvider for the applicaton

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
+import { ClerkProvider } from '@clerk/clerk-expo'
 import FontAwesome from '@expo/vector-icons/FontAwesome'
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
 
 export {
 	// Catch any errors thrown by the Layout component.
@@ -41,24 +45,46 @@ export default function RootLayout() {
 	return <RootLayoutNav />
 }
 
+const tokenCache = {
+	async getToken(key: string) {
+		try {
+			return SecureStore.getItemAsync(key)
+		} catch (err) {
+			return null
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value)
+		} catch (err) {
+			return
+		}
+	}
+}
+
 function RootLayoutNav() {
 	return (
-		<Stack>
-			<Stack.Screen name='index' options={{ headerShown: false }} />
-			<Stack.Screen
-				name='otp'
-				options={{
-					headerTitle: 'Enter Your Phone Number',
-					headerBackVisible: false
-				}}
-			/>
-			<Stack.Screen
-				name='verify/[phone]'
-				options={{
-					headerTitle: 'Verify Your Phone Number',
-					headerBackTitle: 'Edit Number'
-				}}
-			/>
-		</Stack>
+		<ClerkProvider
+			publishableKey={CLERK_PUBLISHABLE_KEY!}
+			tokenCache={tokenCache}
+		>
+			<Stack>
+				<Stack.Screen name='index' options={{ headerShown: false }} />
+				<Stack.Screen
+					name='otp'
+					options={{
+						headerTitle: 'Enter Your Phone Number',
+						headerBackVisible: false
+					}}
+				/>
+				<Stack.Screen
+					name='verify/[phone]'
+					options={{
+						headerTitle: 'Verify Your Phone Number',
+						headerBackTitle: 'Edit Number'
+					}}
+				/>
+			</Stack>
+		</ClerkProvider>
 	)
 }
