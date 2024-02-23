43e7c5bee563e98d5cae687d0146c172ed3af56c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-23 09:31:42 +0300

    Created an index component for the tabs organizational folder

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const TabsIndexPage = () => {
+	return (
+		<View>
+			<Text>INSIDE</Text>
+		</View>
+	)
+}
+
+export default TabsIndexPage
